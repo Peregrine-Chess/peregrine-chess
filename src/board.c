a4ae89dd6639398528d1ba136a1e37cb7fc32468
Adam Salhi <adam06genius@gmail.com>
2021-08-05 12:06:14 +0000

    Finished FEN string parsing

--- src/board.c
+++ src/board.c
@@ -59,34 +59,84 @@ void printboard() {
 }
 
 void parsefen(char *fen) {
-    // reset everything
     memset(bitboards, 0ULL, sizeof(bitboards));
     memset(occupancies, 0ULL, sizeof(occupancies));
+
     side = 0;
     en_passant = no_sq;
     castle = 0;
-
-    for (int rank = 0; rank < 8; rank ++) {
-        for (int file = 0; file < 8; file ++) {
+    
+    for (int rank = 0; rank < 8; rank++) {
+        for (int file = 0; file < 8; file++) {
             int square = rank * 8 + file;
-
+        
             if ((*fen >= 'a' && *fen <= 'z') || (*fen >= 'A' && *fen <= 'Z')) {
                 int piece = char_pieces[*fen];
-                set_bit(bitboards[piece], square);
-                *fen ++;
-            }
 
+                set_bit(bitboards[piece], square);
+
+                *fen++;
+            }
+            
             if (*fen >= '0' && *fen <= '9') {
                 int offset = *fen - '0';
 
+                int piece = -1;
+
+                for (int bb_piece = P; bb_piece <= k; bb_piece++) {
+                    if (get_bit(bitboards[bb_piece], square))
+                        piece = bb_piece;
+                }   
+
+                if (piece == -1)
+                    file--;
+                
                 file += offset;
-
-                *fen ++;
+                
+                *fen++;
             }
-
-            if (*fen == '/') {
-                *fen ++;
-            }
+            
+            if (*fen == '/')
+                *fen++;
         }
     }
+    
+    *fen++;
+
+    (*fen == 'w') ? (side = WHITE) : (side = BLACK);
+
+    fen += 2;
+
+    while (*fen != ' ') {
+        switch (*fen) {
+            case 'K': castle |= WK; break;
+            case 'Q': castle |= WQ; break;
+            case 'k': castle |= BK; break;
+            case 'q': castle |= BQ; break;
+            case '-': break;
+        }
+
+        *fen++;
+    }
+    
+    *fen++;
+
+    if (*fen != '-') {
+        int file = fen[0] - 'a';
+        int rank = 8 - (fen[1] - '0');
+
+        en_passant = rank * 8 + file;
+    }
+
+    else
+        en_passant = no_sq;
+
+    for (int piece = P; piece <= K; piece++)
+        occupancies[WHITE] |= bitboards[piece];
+
+    for (int piece = p; piece <= k; piece++)
+        occupancies[BLACK] |= bitboards[piece];
+    
+    occupancies[BOTH] |= occupancies[WHITE];
+    occupancies[BOTH] |= occupancies[BLACK];
 }
