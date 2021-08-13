9f2e9f6fa1f965fa9270d4a1a60e549a43f5b980
Adam Salhi <adam06genius@gmail.com>
2021-08-13 20:51:38 +0000

    Move encoding/decoding

---
+++ src/include/move.h
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#ifndef __MOVE_H__
+#define __MOVE_H__
+
+#define encode_move(source, target, piece, promoted, capture, double, enpassant, castling) \
+    (source) |          \
+    (target << 6) |     \
+    (piece << 12) |     \
+    (promoted << 16) |  \
+    (capture << 20) |   \
+    (double << 21) |    \
+    (enpassant << 22) | \
+    (castling << 23)    \
+    
+#define get_move_source(move) (move & 0x3f)
+#define get_move_target(move) ((move & 0xfc0) >> 6)
+#define get_move_piece(move) ((move & 0xf000) >> 12)
+#define get_move_promoted(move) ((move & 0xf0000) >> 16)
+#define get_move_capture(move) (move & 0x100000)
+#define get_move_double(move) (move & 0x200000)
+#define get_move_enpassant(move) (move & 0x400000)
+#define get_move_castling(move) (move & 0x800000)
+
+#endif
--- src/main.c
+++ src/main.c
@@ -10,20 +10,27 @@
 #include <board.h>
 #include <misc.h>
 #include <movegen.h>
+#include <move.h>
 
 int main() {
-  init_all(); // INIT *EVERYTHING*
+    // init EVERYTHING
+    init_all();
+    
+    int move = encode_move(d7, e8, P, Q, 1, 0, 0, 0);
 
-    parsefen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ");
-    printboard();
+    int start_square = get_move_source(move);
+    int target_square = get_move_target(move);
+    int piece = get_move_piece(move);
+    int promoted_piece = get_move_promoted(move);
 
-    int move = 0;
+    printf("start square: %s\n", square_to_coords[start_square]);
+    printf("target square: %s\n", square_to_coords[target_square]);
+    printf("piece: %c\n", ascii_pieces[piece]);
+    printf("piece: %c\n", ascii_pieces[promoted_piece]);
+    printf("capture flag: %d\n", get_move_capture(move) ? 1 : 0);
+    printf("double pawn push flag: %d\n", get_move_double(move) ? 1 : 0);
+    printf("enpassant flag: %d\n", get_move_enpassant(move) ? 1 : 0);
+    printf("castling flag: %d\n", get_move_castling(move) ? 1 : 0);
     
-    move = (move | 63) << 6;
-    
-    printbitboard(move);
-    
-    int target_square = (move & 0xfc0) >> 6;
-    
-    printf("target square: %d   %s\n", target_square, square_to_coords[target_square]);
+    return 0;
 }
